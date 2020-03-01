5bf180ef99da2094e077975634bfddae6adc8622
Robert Nelson <robertcnelson@gmail.com>
2020-03-01 11:22:46 -0600

    scripts: resync all build scripts with master repo: https://github.com/RobertCNelson/linux-dev
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 #
-# Copyright (c) 2009-2019 Robert Nelson <robertcnelson@gmail.com>
+# Copyright (c) 2009-2020 Robert Nelson <robertcnelson@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -24,6 +24,18 @@ DIR=$PWD
 git_bin=$(which git)
 
 mkdir -p "${DIR}/deploy/"
+
+config_patch_build_salt () {
+	sed -i -e 's:CONFIG_BUILD_SALT:#CONFIG_BUILD_SALT:g' .config
+	echo "CONFIG_BUILD_SALT=\"\"" >> .config
+}
+
+config_use_lzo_if_no_lz4 () {
+	if [ ! -f /usr/bin/lz4 ] ; then
+		sed -i -e 's:CONFIG_KERNEL_LZ4=y:# CONFIG_KERNEL_LZ4 is not set:g' .config
+		sed -i -e 's:# CONFIG_KERNEL_LZO is not set:CONFIG_KERNEL_LZO=y:g' .config
+	fi
+}
 
 patch_kernel () {
 	cd "${DIR}/KERNEL" || exit
@@ -56,6 +68,11 @@ copy_defconfig () {
 
 make_menuconfig () {
 	cd "${DIR}/KERNEL" || exit
+	if [ ! -f "${DIR}/.yakbuild" ] ; then
+		config_patch_build_salt
+		config_use_lzo_if_no_lz4
+	fi
+	make ARCH=${KERNEL_ARCH} CROSS_COMPILE="${CC}" oldconfig
 	make ARCH=${KERNEL_ARCH} CROSS_COMPILE="${CC}" menuconfig
 	if [ ! -f "${DIR}/.yakbuild" ] ; then
 		cp -v .config "${DIR}/patches/defconfig"
@@ -249,13 +266,15 @@ if [  -f "${DIR}/.yakbuild" ] ; then
 	BUILD=$(echo ${kernel_tag} | sed 's/[^-]*//'|| true)
 fi
 make_kernel
+if [ ! "${AUTO_BUILD_DONT_PKG}" ] ; then
 make_modules_pkg
-if [ -f "${DIR}/KERNEL/scripts/Makefile.fwinst" ] ; then
-	#Finally nuked in v4.14.0-rc0 merge...
-	make_firmware_pkg
-fi
-if grep -q dtbs "${DIR}/KERNEL/arch/${KERNEL_ARCH}/Makefile"; then
-	make_dtbs_pkg
+	if [ -f "${DIR}/KERNEL/scripts/Makefile.fwinst" ] ; then
+		#Finally nuked in v4.14.0-rc0 merge...
+		make_firmware_pkg
+	fi
+	if grep -q dtbs "${DIR}/KERNEL/arch/${KERNEL_ARCH}/Makefile"; then
+		make_dtbs_pkg
+	fi
 fi
 echo "-----------------------------"
 echo "Script Complete"
